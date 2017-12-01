c9695da4082b6a1cb54833c8050858cc9535521b
Mike Peel <email@mikepeel.net>
2017-12-01 19:27:06 -0200

    check more regexes

--- cochrane.py
+++ cochrane.py
@@ -41,38 +41,45 @@ archive.save('Archiving old reports')
 report.text = report_text_new
 report.save('Archiving old reports')
 
-generator = pagegenerators.SearchPageGenerator('insource:/\| *journal *= *.+Cochrane/', site=site, namespaces=[0])
-gen = pagegenerators.PreloadingGenerator(generator)
+regexes = ["insource:/\| journal =.+Cochrane/", "insource:/\| journal=.+Cochrane/", "insource:/\|journal =.+Cochrane/", "insource:/\|journal=.+Cochrane/","insource:/\| title =.+Cochrane/", "title:/\| title=.+Cochrane/", "insource:/\|title =.+Cochrane/", "insource:/\|title=.+Cochrane/"]
+i = 0
+for regex in regexes:
+    generator = pagegenerators.SearchPageGenerator(regex, site=site, namespaces=[0])
+    gen = pagegenerators.PreloadingGenerator(generator)
 
-for page in gen:
-    # print checkedpages
-    try:
-        text = page.get()
-    except:
-        continue
-    pmids = re.findall(r'\|\s*?pmid\s*?\=\s*?(\d+?)\s*?\|', text)
-    print len(pmids)
-    for pmid in pmids:
-        if str(pmid) not in checkedpages:
-            print 'https://www.ncbi.nlm.nih.gov/pubmed/%s' % pmid
-            try:
-                res = urllib2.urlopen('https://www.ncbi.nlm.nih.gov/pubmed/%s' % pmid).read().decode('utf-8')
-            except:
-                continue
-            if 'WITHDRAWN' in res and re.search(r'<h3>Update in</h3><ul><li class="comments"><a href="/pubmed/\d+?"', res):
-                pm = re.findall(r'<h3>Update in</h3><ul><li class="comments"><a href="/pubmed/(\d+?)"', res)[0]
-                checkedpages[str(pmid)] = pm
+    for page in gen:
+        # print checkedpages
+        # print page
+        i += 1
+        try:
+            text = page.get()
+        except:
+            continue
+        pmids = re.findall(r'\|\s*?pmid\s*?\=\s*?(\d+?)\s*?\|', text)
+        print len(pmids)
+        for pmid in pmids:
+            if str(pmid) not in checkedpages:
+                print 'https://www.ncbi.nlm.nih.gov/pubmed/%s' % pmid
+                try:
+                    res = urllib2.urlopen('https://www.ncbi.nlm.nih.gov/pubmed/%s' % pmid).read().decode('utf-8')
+                except:
+                    continue
+                if 'WITHDRAWN' in res and re.search(r'<h3>Update in</h3><ul><li class="comments"><a href="/pubmed/\d+?"', res):
+                    pm = re.findall(r'<h3>Update in</h3><ul><li class="comments"><a href="/pubmed/(\d+?)"', res)[0]
+                    checkedpages[str(pmid)] = pm
+                else:
+                    checkedpages[str(pmid)] = 0
             else:
-                checkedpages[str(pmid)] = 0
-        else:
-            print 'using cache for ' + str(pmid)
+                print 'using cache for ' + str(pmid)
 
-        if checkedpages[str(pmid)] != 0:
-            if '<!-- No update needed: ' + str(pmid) + ' -->' not in text:
-                up = u'{{Update inline|reason=Updated version https://www.ncbi.nlm.nih.gov/pubmed/' + checkedpages[str(pmid)]
-                if not up in text:
-                    text = re.sub(ur'(\|\s*?pmid\s*?\=\s*?%s\s*?(?:\||\}\}).*?\< *?\/ *?ref *?\>)' % pmid,ur'\1%s}}' % up, text, re.DOTALL)
-                update_report(page, pmid, checkedpages[str(pmid)])
-    if text != page.text:
-        page.text = text
-        page.save(u'Adding "update inline" template for Cochrane reference')
+            if checkedpages[str(pmid)] != 0:
+                if '<!-- No update needed: ' + str(pmid) + ' -->' not in text:
+                    up = u'{{Update inline|reason=Updated version https://www.ncbi.nlm.nih.gov/pubmed/' + checkedpages[str(pmid)]
+                    if not up in text:
+                        text = re.sub(ur'(\|\s*?pmid\s*?\=\s*?%s\s*?(?:\||\}\}).*?\< *?\/ *?ref *?\>)' % pmid,ur'\1%s}}' % up, text, re.DOTALL)
+                    update_report(page, pmid, checkedpages[str(pmid)])
+        if text != page.text:
+            page.text = text
+            page.save(u'Adding "update inline" template for Cochrane reference')
+
+print str(i) + " pages checked!"
