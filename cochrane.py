15585443b6faa9c67b5e514359be757a4246272a
Mike Peel <email@mikepeel.net>
2018-01-03 15:26:24 +0000

    debug

--- cochrane.py
+++ cochrane.py
@@ -110,7 +110,7 @@ for regex in regexes:
                 if '<!-- No update needed: ' + str(pmid) + ' -->' not in text:
                     up = u'{{Update inline|reason=Updated version https://www.ncbi.nlm.nih.gov/pubmed/' + checkedpages[str(pmid)]
                     if not up in text:
-                        text = re.sub(ur'(\|\s*?pmid\s*?\=\s*?%s\s*?(?:\||\}\}).*?\< *?\/ *?ref *?\>)' % pmid,ur'\1%s'+datestr+'}}' % up, text, re.DOTALL)
+                        text = re.sub(ur'(\|\s*?pmid\s*?\=\s*?%s\s*?(?:\||\}\}).*?\< *?\/ *?ref *?\>)' % pmid,ur'\1%s}}' % (up+str(datestr)), text, re.DOTALL)
                     if debug == False:
                         update_report(page, pmid, checkedpages[str(pmid)])
 
