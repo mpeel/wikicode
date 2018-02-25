d27b0f107cf974c3028d4df96a3a5569892968d0
Mike Peel <email@mikepeel.net>
2018-02-25 10:26:59 -0300

    check for interwiki conflicts

--- commonscat_check.py
+++ commonscat_check.py
@@ -129,7 +129,8 @@ for page in generator:
             if nummodified >= maxnum:
                 print 'Reached the maximum of ' + str(maxnum) + ' entries modified, quitting!'
                 print 'Bad commonscats: ' + str(bad_commonscat_count) + ", bad sitelinks:" + str(bad_sitelink_count)
-                print 'Interwiki conflicts in: ' + interwiki_conflicts
+                print 'Interwiki conflicts in: ':
+                print interwiki_conflicts
                 exit()
 
 print 'Done! Edited ' + str(nummodified) + ' entries'
