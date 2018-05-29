24d004074f03d09431a68a65cec4912c96ff4d55
Mike Peel <email@mikepeel.net>
2018-05-29 17:58:02 -0300

    +

--- commons_migrate_ids_to_wikidata.py
+++ commons_migrate_ids_to_wikidata.py
@@ -24,8 +24,8 @@ manual = False
 category = 'Category:Buildings of Madrid with COAM Register number'#'Category:Listed buildings in England with known IDs'
 templates = ['COAM']#['Listed building England', 'listed building England']
 properties = ['P2917']#['P1216', 'P1216']
-others = ['mainw','Mainw', 'Interwiki from Wikidata', 'interwiki from Wikidata', 'label', 'Label']
-enwp = ['mainw', 'Mainw', 'on Wikipedia|en=', 'On Wikipedia|en=', 'object location|wikidata=', 'object location|Wikidata=', 'Object location|Wikidata=', 'Object location|wikidata=', "Interwiki from Wikidata", "interwiki from Wikidata", "Interwiki from wikidata", "interwiki from wikidata", "PeopleByName", "peopleByName", "Authority control", "authority control", "On Wikidata", "on Wikidata", "In Wikidata", "in Wikidata", "Wikidata", "wikidata"]
+others = ['mainw','Mainw', 'Interwiki from Wikidata', 'interwiki from Wikidata', 'label', 'Label', 'object location|wikidata=', 'object location|Wikidata=', 'Object location|Wikidata=', 'Object location|wikidata=', "Interwiki from Wikidata", "interwiki from Wikidata", "Interwiki from wikidata", "interwiki from wikidata", "PeopleByName", "peopleByName", "Authority control", "authority control", "On Wikidata", "on Wikidata", "In Wikidata", "in Wikidata", "Wikidata", "wikidata"]
+enwp = ['mainw', 'Mainw', 'on Wikipedia|en=', 'On Wikipedia|en=']
 savemessage="Trim information provided through the Wikidata Infobox"
 wikidatainfobox = ["Wikidata Infobox", "Wikidata infobox", "wikidata infobox", "wikidata Infobox", "Infobox Wikidata", "infobox Wikidata", "infobox wikidata", "Infobox wikidata"]
 
