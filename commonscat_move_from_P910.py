93ce291d593912d9e06b86b62b488a93bf87ceca
Mike Peel <email@mikepeel.net>
2018-07-14 11:33:52 -0300

    +

--- commonscat_move_from_P910.py
+++ commonscat_move_from_P910.py
@@ -21,7 +21,7 @@ commons = pywikibot.Site('commons', 'commons')
 debug = 1
 query = 'SELECT ?item ?categoryitem ?commonscategory WHERE { ?item wdt:P910 ?categoryitem . ?commonscategory schema:about ?item . ?commonscategory schema:isPartOf <https://commons.wikimedia.org/> . FILTER REGEX(STR(?commonscategory), "https://commons.wikimedia.org/wiki/Category:") . }'
 if debug:
-    query = query + " LIMIT 10000"
+    query = query + " LIMIT 5000"
 
 print query
 
