e4719ae811ad093d0a26aa05a6e0edbf64649ccb
Mike Peel <email@mikepeel.net>
2018-07-16 09:36:39 +0200

    +

--- pibot_daily.sh
+++ pibot_daily.sh
@@ -4,3 +4,5 @@ cd /home/pi/Documents/wikicode/
 /usr/bin/python permissions.py
 /usr/bin/python guardian_obit.py
 /usr/bin/python commonscat_check.py
+/usr/bin/python commonscat_copy_from_P373.py
+/usr/bin/python commonscat_move_from_P910.py
