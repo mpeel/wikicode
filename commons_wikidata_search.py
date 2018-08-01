0b2a9c40a199b047a34b8a250fcaef3eec40d5d8
Mike Peel <email@mikepeel.net>
2018-08-01 11:36:56 -0400

    +

--- commons_wikidata_search.py
+++ commons_wikidata_search.py
@@ -179,10 +179,10 @@ elif usecategory:
             print str(nummodified) + " - " + str(numchecked) + "/" + str(len(seen)) + "/" + str(len(active)) + "/" + str(len(next_active))
 
             # See if there are subcategories that we want to check in the future
-            for result in pagegenerators.SubCategoriesPageGenerator(cat, recurse=False):
-                if result.title() not in seen:
-                    seen.add(result.title())
-                    next_active.add(result.title())
+            # for result in pagegenerators.SubCategoriesPageGenerator(cat, recurse=False):
+                # if result.title() not in seen:
+                    # seen.add(result.title())
+                    # next_active.add(result.title())
         active = next_active
         if nummodified >= maxnum:
             print 'Reached the maximum of ' + str(maxnum) + ' entries modified, quitting!'
