9829c47d8a69e31d2994bbfa6ec12f4903befb6b
Mike Peel <email@mikepeel.net>
2019-09-16 19:21:42 +0000

    +

--- pibot_weekly.sh
+++ pibot_weekly.sh
@@ -3,4 +3,5 @@ source /home/pi/.profile
 cd /home/pi/Documents/wikicode/
 
 /usr/bin/python enwp_commonscat_fix.py
+/usr/bin/python3 simplewp_commonscat_fix.py
 /usr/bin/python commons_wikidata_infobox_tidy.py
