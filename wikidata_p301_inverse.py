aa303e28e08488c61bb288e7bc0eaae96fff9d80
Mike Peel <email@mikepeel.net>
2020-03-26 19:46:48 +0000

    +

--- wikidata_p301_inverse.py
+++ wikidata_p301_inverse.py
@@ -13,7 +13,7 @@ from pywikibot import pagegenerators
 import urllib
 from pibot_functions import *
 
-maxnum = 10
+maxnum = 1000
 nummodified = 0
 
 wikidata_site = pywikibot.Site("wikidata", "wikidata")
