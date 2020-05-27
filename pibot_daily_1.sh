bac01c143087c95968306165f200347a88e68438
Mike Peel <email@mikepeel.net>
2020-05-27 20:43:48 +0100

    Split daily task into 3 parts

--- pibot_daily_1.sh
+++ pibot_daily_1.sh
@@ -6,7 +6,6 @@ cd /home/pi/Documents/wikicode/
 /usr/bin/python permissions.py
 /usr/bin/python guardian_obit.py
 /usr/bin/python nyt_obit.py
-/usr/bin/python3 wir_newpages_duplicity.py
 /usr/bin/python wikidata_bad_p373.py
 /usr/bin/python commons_defaultsort_conflicts.py
 /usr/bin/python commonscat_move_from_P910.py
@@ -14,8 +13,4 @@ cd /home/pi/Documents/wikicode/
 /usr/bin/python doublecheck_move.py
 /usr/bin/python commonscat_check.py
 /usr/bin/python commonscat_p910_tidy.py
-/usr/bin/python commonscat_copy_from_P373.py
 /usr/bin/python3 wikidata_p301_inverse.py
-
-
-
