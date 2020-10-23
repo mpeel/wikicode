017875088b249afa53da0a20ddf3a88e2e7a160a
Mike Peel <email@mikepeel.net>
2020-10-23 20:00:34 +0100

    +

--- touch_wlm_brazil.sh
+++ touch_wlm_brazil.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source /home/pi/.profile
-export PYTHONPATH=/home/pi/Documents/core:$PYTHONPATH
-cd /home/pi/Documents/wikicode/
+export PYTHONPATH=/home/pi/Documents/git/core:$PYTHONPATH
+source /home/pi/.bashrc
+cd /home/pi/Documents/git/wikicode/
 
 /usr/bin/python3 touch_wlm_brazil.py
