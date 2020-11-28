04d539ca580d0ad637bbecc03772b64025821caa
Mike Peel <email@mikepeel.net>
2020-11-28 16:03:18 +0000

    +

--- shortdesc_functions.py
+++ shortdesc_functions.py
@@ -5,6 +5,7 @@
 # Mike Peel     05-Sep-2020     v1 - start function file
 # Mike Peel     12-Sep-2020     v2 - expanding
 # Mike Peel     27-Nov-2020     v3 - split generator into separate function
+# Mike Peel     28-Nov-2020     v4 - adding required_words, excluded_words, only_one_infobox options, and more
 
 import pywikibot
 from pywikibot import pagegenerators
@@ -13,7 +14,7 @@ import dateparser
 import re
 import time
 
-def shortdesc_generator(wikipedia, page, debug, trip, startpoint, endpoint, require_infobox, infobox_strings, description, add_birth_date, add_death_date):
+def shortdesc_generator(wikipedia, page, debug, trip, startpoint, endpoint, require_infobox, infobox_strings, only_one_infobox, description, add_birth_date, add_death_date,required_words,excluded_words):
 	enwiki_description = ''
 	has_infobox = False
 
@@ -32,13 +33,26 @@ def shortdesc_generator(wikipedia, page, debug, trip, startpoint, endpoint, requ
 
 	if require_infobox:
 		for option in infobox_strings:
-			if option in page.text:
+			if option in page.text.lower():
 				has_infobox = True
 		if not has_infobox:
 			# The article does not have an infobox and we are only looking for articles that do have one.
 			print(page.title() + ' - does not have infobox')
 			return ''
+		if only_one_infobox:
+			if count_infoboxes(page) > 1:
+				print(page.title() + ' - has multiple infoboxes')
+				return ''
 
+	intro_sentence = get_intro_sentence(page)
+	for required in required_words:
+		if required not in intro_sentence:
+			print(page.title() + ' - does not mention ' + required)
+			return ''
+	for excluded in excluded_words:
+		if excluded in intro_sentence:
+			print(page.title() + ' - mentions ' + excluded)
+			return ''
 	enwiki_description = description
 	birthdate = calculateBirthDateFull(page=page).strip()
 	deathdate = calculateDeathDateFull(page=page).strip()
@@ -53,7 +67,7 @@ def shortdesc_generator(wikipedia, page, debug, trip, startpoint, endpoint, requ
 	return enwiki_description
 
 # This is the main funciton to generate and save new short descriptions.
-def shortdesc_stage(targetcat, maxnum, maxnum_new, debug, trip, startpoint, endpoint, require_infobox, infobox_strings, description, add_birth_date, add_death_date,onwiki_page,local_file):
+def shortdesc_stage(targetcat, maxnum, maxnum_new, debug, trip, startpoint, endpoint, require_infobox, infobox_strings, only_one_infobox, description, add_birth_date, add_death_date,required_words,excluded_words,onwiki_page,local_file):
 	# Initialising parameters
 	count = 0
 	count_new = 0
@@ -80,16 +94,19 @@ def shortdesc_stage(targetcat, maxnum, maxnum_new, debug, trip, startpoint, endp
 		# Reset/increment parameters for each loop
 		count += 1
 
-		enwiki_description = shortdesc_generator(wikipedia, page, debug, trip, startpoint, endpoint, require_infobox, infobox_strings, description, add_birth_date, add_death_date)
+		enwiki_description = shortdesc_generator(wikipedia, page, debug, trip, startpoint, endpoint, require_infobox, infobox_strings, only_one_infobox, description, add_birth_date, add_death_date,required_words,excluded_words)
 
 		if enwiki_description != '':
+			wikidata_description = get_wikidata_desc(page)
+			intro_sentence = get_intro_sentence(page)
+
 			# We have a new description, save it into the on-wikip page or a text file
 			count_new += 1
 			if onwiki_page != '':
 				output += '|-\n'
-				output += '| [['+page.title()+"]] || " + enwiki_description+"\n"
+				output += '| [['+page.title()+"]] || " + enwiki_description + " || " + wikidata_description + " || " + intro_sentence + " || " + "\n"
 			else:
-				output += page.title() + ' || ' + enwiki_description+"\n"
+				output += page.title() + ' || ' + enwiki_description + " || " + wikidata_description + " || " + intro_sentence + " || " + "\n"
 			print(page.title() + ' - NEW SHORT DESCRIPTION')
 
 		if count >= maxnum or count_new >= maxnum_new:
@@ -130,7 +147,7 @@ def shortdesc_add(debug,onwiki_page,local_file,wait_time, also_wikidata):
 
 	# Work through them one by one
 	for line in todo:
-		print(line)
+		# print(line)
 		# Only if we don't have a table header
 		if '{|' not in line and '|}' not in line and line.strip() != '':
 			values = line.split('||')
@@ -138,28 +155,15 @@ def shortdesc_add(debug,onwiki_page,local_file,wait_time, also_wikidata):
 			test = 'y'
 			if debug == True:
 				test = 'n'
-				test = input('Add ' + values[1].strip() + ' to https://en.wikipedia.org/wiki/' + values[0].strip().replace(' ','_') + "? ('y' to apply)")
+				test = input('Add "' + values[1].strip() + '"" to https://en.wikipedia.org/wiki/' + values[0].strip().replace(' ','_') + "? ('y' to apply)")
 			if test == 'y':
 				count += 1
 				page.text = '{{Short description|'+values[1].strip()+'}}\n'+page.text
-				page.save('Adding short description ("'+values[1].strip()+'")')
+				page.save('Adding short description ("'+values[1].strip()+'")',minor=False)
 
 			if also_wikidata:
 				# We also want to add the description to Wikidata, if there isn't one there already, and we have a linked Wikidata item
-				try:
-					wd_item = pywikibot.ItemPage.fromPage(page)
-					item_dict = wd_item.get()
-					qid = wd_item.title()
-				except:
-					print('Huh - no page found')
-					continue
-
-				# Get the description from Wikidata to make sure it's empty
-				wikidata_description = ''
-				try:
-					wikidata_description = item_dict['descriptions']['en']
-				except:
-					null = 0
+				wikidata_description = get_wikidata_desc(page)
 
 				# Save the new description to Wikidata
 				if wikidata_description == '':
@@ -176,6 +180,42 @@ def shortdesc_add(debug,onwiki_page,local_file,wait_time, also_wikidata):
 	print("Added descriptions to " + str(count) + " enwp articles and " + str(count2) + ' Wikidata items!')
 	return
 
+# Get the description from Wikidata
+def get_wikidata_desc(page):
+	try:
+		wd_item = pywikibot.ItemPage.fromPage(page)
+		item_dict = wd_item.get()
+		qid = wd_item.title()
+	except:
+		print('Huh - no page found')
+		return ''
+
+	# Get the description from Wikidata to make sure it's empty
+	wikidata_description = ''
+	try:
+		return item_dict['descriptions']['en']
+	except:
+		return ''
+
+# Try to get the first sentence of the article
+def get_intro_sentence(page):
+	sections = pywikibot.textlib.extract_sections(page.text)
+	text = sections[0]
+	text = re.sub('{{.*?}}','',text, flags=re.DOTALL)
+	if "}}" in text:
+		text = text.split("}}")[-1]
+	text = re.sub('<ref.*?</ref>','',text, flags=re.DOTALL)
+	start = text.split('.')[0]
+	start = start.replace("[[",'').replace(']]','').replace('|',' ')
+	return start.strip()
+
+def count_infoboxes(page):
+	count = 0
+	templates = pywikibot.textlib.extract_templates_and_params(page.text)
+	for template in templates:
+		if 'infobox' in template[0].lower():
+			count += 1
+	return count
 
 # This function fetches the page info from a specific item
 def get_pageinfo(site, itemtitle):
--- shortdesc_run.py
+++ shortdesc_run.py
@@ -5,6 +5,7 @@
 # Mike Peel     08-Aug-2020     v1 - start
 # Mike Peel     05-Sep-2020     v2 - rewrite to have more options, moving operational code into shortdesc_functions.py
 # Mike Peel     12-Sep-2020     v3 - expanding
+# Mike Peel     28-Nov-2020     v4 - adding required_words, excluded_words, only_one_infobox options
 
 # To run this code you need to have pywikibot, dateparser and re installed and in your python path. This is python3 code. Set the configuration options below, then run the script. You can select which part of the script you want to run by commenting/uncommenting shortdesc_stage and shortdesc_add. The first function prepares the new short descriptions and stages them, the second function saves them to enwp.
 
@@ -13,11 +14,11 @@ import pywikibot
 from shortdesc_functions import *
 
 # This is the enwp category you want to look at
-targetcat = 'Category:English footballers'
+targetcat = 'Category:Spanish footballers'
 
 # The maximum number of new short descriptions to stage. 'maxnum' is the number of articles to look through, 'maxnum_new' is the number of new short descriptions to stage, the code stops at whichever comes first.
 maxnum = 100
-maxnum_new = 2
+maxnum_new = 1
 
 # Set to 'True' to disable all automatic page editing, 'False' otherwise. If it is 'True' then you'll need to reply 'y' or 'n' to prompts.
 debug = True
@@ -28,11 +29,12 @@ startpoint = ''
 endpoint = ''
 
 # If the articles must require an infobox (as defined by the next line)
-require_infobox = False 
-infobox_strings = ['nfobox']
+require_infobox = True 
+infobox_strings = ['infobox']
+only_one_infobox = True # Only use pages with one infobox
 
 # Options for what to include in the short description. Starts with a base string, with options that can be added afterwards.
-description = 'English footballer'
+description = 'Spanish footballer'
 add_birth_date = True
 add_death_date = True
 
@@ -40,16 +42,20 @@ add_death_date = True
 onwiki_page = ''#'User:Mike Peel/shortdesc'
 local_file = 'test.txt'
 
+# Check the first sentence to make sure it has or hasn't got these words in it
+required_words = ['foot']
+excluded_words = ['blah']
+
 # Here you can test individual articles if you want.
-wikipedia = pywikibot.Site('en', 'wikipedia')
-page = pywikibot.Page(wikipedia, 'Robert Dowland')
-print(shortdesc_generator(wikipedia, page, debug, trip, startpoint, endpoint, require_infobox, infobox_strings, description, add_birth_date, add_death_date))
+# wikipedia = pywikibot.Site('en', 'wikipedia')
+# page = pywikibot.Page(wikipedia, 'Robert Dowland')
+# print(shortdesc_generator(wikipedia, page, debug, trip, startpoint, endpoint, require_infobox, infobox_strings, only_one_infobox, description, add_birth_date, add_death_date, required_words, excluded_words))
 # Comment out this next line to continue further
-exit()
+# exit()
 
 
 # Run staging code
-shortdesc_stage(targetcat, maxnum, maxnum_new, debug, trip, startpoint, endpoint, require_infobox, infobox_strings, description, add_birth_date, add_death_date,onwiki_page,local_file)
+shortdesc_stage(targetcat, maxnum, maxnum_new, debug, trip, startpoint, endpoint, require_infobox, infobox_strings, only_one_infobox, description, add_birth_date, add_death_date,required_words,excluded_words,onwiki_page,local_file)
 
 # Check if we want to make the edits
 input('Check the staging output please, then press return to save the edits')
