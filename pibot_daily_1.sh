45c7a176031ff6825073285b034756b3b274e5f5
Mike Peel <email@mikepeel.net>
2020-12-22 20:14:15 +0000

    +

--- pibot_daily_1.sh
+++ pibot_daily_1.sh
@@ -9,6 +9,7 @@ cd /home/pi/Documents/git/wikicode/
 /usr/bin/python3 nyt_obit.py
 /usr/bin/python3 wikidata_bad_p373.py
 /usr/bin/python3 commons_defaultsort_conflicts.py
+/usr/bin/python3 enwp_commonscat_import.py
 /usr/bin/python3 commonscat_p910_tidy.py
 /usr/bin/python3 commonscat_move_from_P910.py
 /usr/bin/python3 commonscat_move_from_P1754.py
--- pibot_daily_3.sh
+++ pibot_daily_3.sh
@@ -4,5 +4,6 @@ source /home/pi/.bashrc
 export PYTHONPATH=/home/pi/Documents/git/core:$PYTHONPATH
 cd /home/pi/Documents/git/wikicode/
 
+/usr/bin/python3 enwp_commonscat_import.py
 /usr/bin/python3 commonscat_copy_from_P373.py
 /usr/bin/python3 wikidata_import_labels_from_commons.py
