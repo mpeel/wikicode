c33a20fac1ad68901586da6d278a5e919325969e
Mike Peel <email@mikepeel.net>
2021-01-27 11:48:27 +0000

    +

--- enwp_coords.py
+++ enwp_coords.py
@@ -45,7 +45,7 @@ repo = wiki.data_repository()
 globe_item = pywikibot.ItemPage(repo, 'Q2')
 debug = False
 numedited = 0
-maxnumedited = 100
+maxnumedited = 1000
 
 cat = pywikibot.Category(wiki, 'Category:Coordinates not on Wikidata')
 coord_templates = ['Coord']
