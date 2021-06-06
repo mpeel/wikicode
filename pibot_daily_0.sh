b691f49287c6288d19a20a31d5e1f6fcf5c428cb
Mike Peel <email@mikepeel.net>
2021-06-06 14:08:06 +0100

    +

--- pibot_daily_0.sh
+++ pibot_daily_0.sh
@@ -8,3 +8,4 @@ cd /home/pi/Documents/git/wikicode/
 /usr/bin/python3 wikidata_new_from_wikipedia_query_article.py
 /usr/bin/python3 wikidata_new_from_wikipedia_query_category.py
 /usr/bin/python3 enwp_coords.py
+/usr/bin/python3 commons_category_coords.py
