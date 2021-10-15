67f7330a9ac22ecb3b6ac4e23751989fbdfc3369
Mike Peel <email@mikepeel.net>
2021-10-15 17:08:15 +0100

    +

--- pibot_daily_0.sh
+++ pibot_daily_0.sh
@@ -5,6 +5,7 @@ export PYTHONPATH=/home/pi/Documents/git/core:$PYTHONPATH
 cd /home/pi/Documents/git/wikicode/
 
 /usr/bin/python3 commons_wikidata_infobox.py
+/usr/bin/python3 wikidata_bot_requests.py
 /usr/bin/python3 wikidata_new_from_wikipedia_query_article.py
 /usr/bin/python3 wikidata_new_from_wikipedia_query_category.py
 /usr/bin/python3 enwp_coords.py
