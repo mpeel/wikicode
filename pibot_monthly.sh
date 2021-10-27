7cca5acbd6082a1c8e3fdc391e2618a61d8d3dcd
Mike Peel <email@mikepeel.net>
2021-10-27 17:34:21 +0100

    +

--- pibot_monthly.sh
+++ pibot_monthly.sh
@@ -4,6 +4,7 @@ source /home/pi/.bashrc
 export PYTHONPATH=/home/pi/Documents/git/core:$PYTHONPATH
 cd /home/pi/Documents/git/wikicode/
 
+/usr/bin/python3 commons_date_find.py
 /usr/bin/python3 cochrane.py
 /usr/bin/python3 cochrane_fr.py
 # Run this twice to catch cases where the IMO category was moved
