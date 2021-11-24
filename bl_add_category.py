ac1d7c76c05b869ba2e83f89305343e4bd5eaed6
Mike Peel <email@mikepeel.net>
2021-11-24 17:05:11 +0000

    +

---
+++ bl_add_category.py
@@ -0,0 +1,46 @@
+from __future__ import unicode_literals
+#!/usr/bin/python
+# -*- coding: utf-8  -*-
+# Add some text to a wiki page
+# Mike Peel     11-Jul-2017     v1 - initial version
+# Mike Peel		24-Nov-2021		v2 - British Library specific code
+
+import pywikibot
+import numpy as np
+import time
+import string
+from pywikibot import pagegenerators
+import time
+
+# Settings
+targetcat = "Category:British Library King's Topographical collection"
+newcat = "Category:Uploaded by the British Library"
+nummodified = 0
+maxnum = 1
+
+# Connect to the wikis
+site = pywikibot.Site('commons', 'commons')
+repo = site.data_repository()  # this is a DataSite object
+
+def addcat(page, newcat):
+		if page.text == '':
+			print("Error - page is empty!")
+			return 0
+		elif newcat not in page.text:
+			page.text = page.text + "\n[[" + newcat + "]]"
+			page.save(u"Adding [[:" + newcat + "]]")
+			return 1
+		else:
+			print("Already in category")
+			return 0
+
+cat = pywikibot.Category(site, targetcat)
+pages = pagegenerators.CategorizedPageGenerator(cat, recurse=False);
+for page in pages:
+	print(page.title())
+	nummodified += addcat(page, newcat)
+
+	print(nummodified)
+	if nummodified >= maxnum:
+		print("Reached the maximum of " + str(maxnum) + " entries modified, quitting!")
+		exit()
