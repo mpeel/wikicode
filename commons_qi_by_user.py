dcb8ceb9dda2183ca7b7388352d2dc6fcd1ed2b8
Mike Peel <email@mikepeel.net>
2022-01-15 21:48:27 +0000

    QI by user automation

---
+++ commons_qi_by_user.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Quality Image categories by user
+# Mike Peel     15-Jan-2022      v1 - start
+# Import modules
+import pywikibot
+from pywikibot import pagegenerators
+
+commons = pywikibot.Site('commons', 'commons')
+
+targetcat = 'Category:Quality images by user'
+destinationpage = 'Commons:Quality images by user'
+skip = ['by country', 'by subject', 'images from']
+
+
+page = pywikibot.Page(commons, destinationpage)
+pagetext = 'This is a list of [[Commons:Quality images|quality images]] by user. If you want to be included in this list, create a subcategory of [[:Category:Quality images by user]] with the format \'Quality images by <username>\', and you will be included in this list with the next bot update (daily). This page is automatically updated by [[User:Pi bot]]. If you want to change the format of this page, or want to be excluded from this list, please contact [[User:Mike Peel]]. Manual changes will be ignored by the bot update.\n== {{LangSwitch|cs=Tabulka|de=Tabelle|en=Table|zh=表格}} ==\n\n{|class="wikitable sortable" cellspacing="0"\n!{{LangSwitch|de=Benutzer|en=User|zh=用戶}}\n!{{LangSwitch|de=Kategorie|en=Category|zh=分類}}\n!{{LangSwitch|de=Anzahl QIs|en=Number QIs|zh=優質圖像數量}}\n'
+cat = pywikibot.Category(commons, targetcat)
+subcats = pagegenerators.SubCategoriesPageGenerator(cat, recurse=False);
+for subcat in subcats:
+	skipthis = False
+	for toskip in skip:
+		if toskip in subcat.title():
+			skipthis = True
+	if skipthis:
+		continue
+	print(subcat.title())
+	title = subcat.title()
+	title = title.replace('By','by')
+	title = title.replace('user','User')
+	print(title)
+	if "'s" in title:
+		username = title.split("'s")[0]
+	elif " - " in title:
+		username = title.split("-")[0]
+	elif "/" in title:
+		username = title.split("/")[0]
+	else:
+		username = title.split('by ')[1]
+	username = username.replace('User:','')
+	username = username.replace('Photographs by','')
+	username = username.replace('Files by','')
+	username = username.replace('Category:','')
+	if '(' in username:
+		username = username.split('(')[0]
+	username = username.strip()
+	if '/' in username:
+		username = username.split('/')[0]
+	pagetext = pagetext + '|-\n'+'|[[User:'+username+'|'+username+']] || [[:'+subcat.title() + '|' + subcat.title().replace('Category:','') + ']] || align="right" | {{PAGESINCAT:'+subcat.title().replace('Category:','') + '|files}}\n'
+
+pagetext = pagetext + '\n|}[[Category:Quality images by user| ]]'
+page.text = pagetext
+page.save('Updating')
--- pibot_daily_3.sh
+++ pibot_daily_3.sh
@@ -4,6 +4,7 @@ source /home/pi/.bashrc
 export PYTHONPATH=/home/pi/Documents/git/core:$PYTHONPATH
 cd /home/pi/Documents/git/wikicode/
 
+/usr/bin/python3 commons_qi_by_user.py
 /usr/bin/python3 enwp_commonscat_import.py
 /usr/bin/python3 commonscat_copy_from_P373.py
 /usr/bin/python3 wikidata_import_labels_from_commons.py
