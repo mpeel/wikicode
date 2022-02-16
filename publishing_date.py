8649b440fc2dbe1842db9863db33bf796964bc4d
Mahfuza Humayra Mohona <mhmohona@gmail.com>
2022-02-16 12:58:17 +0600

    Check Online dictionaries catagory

--- publishing_date.py
+++ publishing_date.py
@@ -2,29 +2,56 @@ import pandas as pd
 import re 
 import wikipedia
 import pywikibot 
+import requests
+from pywikibot import pagegenerators
 
-ptwiki = pywikibot.Site('wikidata', 'wikidata') 
-ptwiki_repo = ptwiki.data_repository() 
+wiki = pywikibot.Site('wikidata', 'wikidata') 
+wiki_repo = wiki.data_repository() 
 
-article_name = "Oxford Dictionary of World Religions"
-QID = "Q7115313"
+enwiki = pywikibot.Site('en', 'wikipedia')
+enwiki_repo = enwiki.data_repository() 
 
-article = wikipedia.page(article_name).content
-wikidata_item = pywikibot.ItemPage(ptwiki_repo, QID)
-item_dict = wikidata_item.get() 
+# article_name = "Oxford Dictionary of World Religions"
+# QID = "Q7115313"
 
-try:
-    test = item_dict['claims']['P577']
-    print('This article has publication date in Wikidata Item. ')
-except:
-    # todo: have to create new item 
-    print ('This article does not have publication date in Wikidata Item. ')
+targetcat = 'Category:Online dictionaries'
+cat = pywikibot.Category(enwiki, targetcat)
 
-article = wikipedia.page("Oxford Dictionary of World Religions").content
+def wiki_title_to_wikidata_id(title: str) -> str:
+        protocol = 'https'
+        base_url = 'en.wikipedia.org/w/api.php'
+        params = f'action=query&prop=pageprops&format=json&titles={title}'
+        url = f'{protocol}://{base_url}?{params}'
+        
+        response = requests.get(url)
+        json = response.json()
+        for pages in json['query']['pages'].values():
+            wikidata_id = pages['pageprops']['wikibase_item']
+        return wikidata_id
 
-for item in article.split("."):
-    if "published" in item:
-        line_with_date = item.strip()
+subpages = pagegenerators.CategorizedPageGenerator(cat, recurse=False);
+for subpage in subpages:
+    article_name = subpage.title()
+    # print (article_name)   
+    QID = wiki_title_to_wikidata_id(subpage.title())
+    # print(QID)          
+
+    article = wikipedia.page(article_name).content
+    wikidata_item = pywikibot.ItemPage(wiki_repo, QID)
+    item_dict = wikidata_item.get() 
+
+    try:
+        test = item_dict['claims']['P577']
+        print(article_name + 'has publication date in Wikidata Item.')
+    except:
+        # todo: have to create new item 
+        print (article_name + 'does not have publication date in Wikidata Item.')
+
+        for item in article.split("."):
+            if "published" in item:
+                #check synonims for publish
+                line_with_date = item.strip()
+                print(line_with_date)
 
 # extracting date variants
 def date_extractor(line_with_date): 
@@ -48,6 +75,7 @@ def date_extractor(line_with_date):
     search3 = dict()
     for ind,vals in dict(df.apply(lambda x:re.search(r'\d{1,2}[/-]\d{4}',x,re.M|re.I))).items():
         if vals and (ind not in (list(search1.keys()) + list(search2.keys()))):
+
             search3[ind]=vals.group()
 
     # 2009; 2010
