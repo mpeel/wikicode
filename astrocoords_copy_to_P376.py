dc07e3cefe42c67a0ac7d356d545b9b553283475
Mike Peel <email@mikepeel.net>
2022-08-12 16:33:22 +0100

    expand globes

--- astrocoords_copy_to_P376.py
+++ astrocoords_copy_to_P376.py
@@ -9,12 +9,9 @@ import string
 from pywikibot import pagegenerators
 import urllib
 
-# globe_names = ['Earth', 'Mars', 'Moon', 'Ceres', 'Mercury', 'Venus', 'phobos', 'deimos', 'ganymede', 'callisto', 'io', 'europa', 'mimas', 'enceladus', 'tethys', 'dione', 'rhea', 'titan', 'hyperion', 'iapetus', 'phoebe', 'miranda', 'ariel', 'umbriel', 'titania', 'oberon', 'triton', 'pluto']
-globes = ['Q111', 'Q405', 'Q596', 'Q308', 'Q313', 'Q7547', 'Q7548', 'Q3169', 'Q3134', 'Q3123', 'Q3143', 'Q15034', 'Q3303', 'Q15047', 'Q15040', 'Q15050', 'Q2565', 'Q15037', 'Q17958', 'Q17975', 'Q3352', 'Q3343', 'Q3338', 'Q3322', 'Q3332', 'Q3359', 'Q339'] # 'Q2',
-
+globes = ['Q111', 'Q193', 'Q308', 'Q313', 'Q319', 'Q324', 'Q339', 'Q405', 'Q596', 'Q2565', 'Q3030', 'Q3123', 'Q3134', 'Q3143', 'Q3169', 'Q3257', 'Q3303', 'Q3322', 'Q3332', 'Q3338', 'Q3343', 'Q3352', 'Q3359', 'Q6604', 'Q7547', 'Q7548', 'Q11558', 'Q15034', 'Q15037', 'Q15040', 'Q15047', 'Q15050', 'Q15662', 'Q16081', 'Q16711', 'Q16765', 'Q17751', 'Q17754', 'Q17958', 'Q17975', 'Q107556', 'Q149012', 'Q149374', 'Q149417', 'Q150249', 'Q158244', 'Q510728', 'Q844672', 'Q1385178']
 commons = pywikibot.Site('commons', 'commons')
 repo = commons.data_repository()
-debug = False
 
 for i in range(0,len(globes)):
 	globe_item = pywikibot.ItemPage(repo, globes[i])
@@ -41,9 +38,4 @@ for i in range(0,len(globes)):
 		if P376 == '':
 			newclaim = pywikibot.Claim(repo, 'P376')
 			newclaim.setTarget(globe_item)
-			if debug:
-				text = input("Save link? ")
-			else:
-				text = 'y'
-			if text != 'n':
-				wd_item.addClaim(newclaim, summary=u'Adding P376 value to match coordinate globe')
+			wd_item.addClaim(newclaim, summary=u'Adding P376 value to match coordinate globe')
