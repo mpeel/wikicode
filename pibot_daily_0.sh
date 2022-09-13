3e77cc1500eff9e21e02e58c59980b8c4ef4e38d
Mike Peel <email@mikepeel.net>
2022-09-13 19:50:09 +0100

    tweak

--- pibot_daily_0.sh
+++ pibot_daily_0.sh
@@ -4,6 +4,7 @@ source /home/pi/.bashrc
 export PYTHONPATH=/home/pi/Documents/git/core:$PYTHONPATH
 cd /home/pi/Documents/git/wikicode/
 
+/usr/bin/python3 commons_wikidata_infobox_tidy.py
 /usr/bin/python3 commons_wikidata_infobox.py
 /usr/bin/python3 wikidata_bot_requests.py
 /usr/bin/python3 wikidata_new_from_wikipedia_query_article.py
