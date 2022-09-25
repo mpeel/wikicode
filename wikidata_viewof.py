d137cb90351ff55a00d2d60a3bc00d7275ea9da2
Mike Peel <email@mikepeel.net>
2022-09-25 19:12:17 +0100

    fix

--- wikidata_viewof.py
+++ wikidata_viewof.py
@@ -58,7 +58,7 @@ def newitem(category, items,cat=True):
 			print("That didn't work")
 	return candidate_item
 
-searchstrings = [ "- Exterior", "- Interior)", ":Exterior_of", ":Interior_of", ":View_of",":Views_of",":View_from",":Views_from", "(exterior)", "(interior)"]
+searchstrings = [ "- Exterior", "- Interior", ":Exterior_of", ":Interior_of", ":View_of",":Views_of",":View_from",":Views_from", "(exterior)", "(interior)"]
 properties = [ 'P8596', 'P7561', 'P8596', 'P7561', 'P8989','P8989','P8933','P8933','P8596', 'P7561']
 combines = ['Q1385033','Q2998430', 'Q1385033','Q2998430','Q2075301','Q2075301','Q2075301','Q2075301','Q1385033','Q2998430']
 for k in range(0,len(searchstrings)):
