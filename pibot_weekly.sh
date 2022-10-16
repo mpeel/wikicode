ede98ed4c21e05620a93757f2872078d0b9afc14
Mike Peel <email@mikepeel.net>
2022-10-16 12:43:28 +0100

    update

--- pibot_weekly.sh
+++ pibot_weekly.sh
@@ -7,9 +7,10 @@ cd /home/pi/Documents/git/wikicode/
 /usr/bin/python3 enwp_commonscat_fix.py
 /usr/bin/python3 simplewp_commonscat_fix.py
 /usr/bin/python3 wikidata_import_infobox_qid.py
-/usr/bin/python3 commons_wikidata_infobox_tidy.py
 /usr/bin/python3 enwp_wikidata_import_shortdesc.py
 /usr/bin/python3 commonscat_check.py
 /usr/bin/python3 wikidata_en_biography_names.py
 /usr/bin/python3 wikidata_pt_biography_names.py
 /usr/bin/python3 wikidata_brackets_in_biography_names.py
+/usr/bin/python3 wikidata_new_from_wikipedia_query_article_nocreate.py
+/usr/bin/python3 wikidata_new_from_wikipedia_query_category_nocreate.py
