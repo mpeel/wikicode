f84722c332ccafe7543fce042980680b351b4881
Mike Peel <email@mikepeel.net>
2022-11-01 17:52:15 +0000

    reorder

--- pibot_monthly.sh
+++ pibot_monthly.sh
@@ -4,11 +4,11 @@ source /home/pi/.bashrc
 export PYTHONPATH=/home/pi/Documents/git/core:$PYTHONPATH
 cd /home/pi/Documents/git/wikicode/
 
-/usr/bin/python3 commons_date_find.py
-/usr/bin/python3 cochrane.py
-/usr/bin/python3 cochrane_fr.py
 # Run this twice to catch cases where the IMO category was moved
 /usr/bin/python3 wikidata_newshipname.py
 /usr/bin/python3 wikidata_newshipname.py
 /usr/bin/python3 wikidata_viewof.py
 /usr/bin/python3 commonscat_redirects.py
+/usr/bin/python3 commons_date_find.py
+/usr/bin/python3 cochrane.py
+/usr/bin/python3 cochrane_fr.py
