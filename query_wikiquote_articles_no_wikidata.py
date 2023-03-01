517f7af53d0ef843880a19c57ce9bccd9dd2ab46
Mike Peel <email@mikepeel.net>
2023-03-01 21:09:09 +0000

    +

--- query_wikiquote_articles_no_wikidata.py
+++ query_wikiquote_articles_no_wikidata.py
@@ -36,6 +36,6 @@ for lang in languages:
 		ftp = FTP('mikepeel.net',user=ftpuser,passwd=ftppass)
 		ftp.cwd('wiki')
 		file = open('/data/project/pibot/'+lang+'wquote_articles.csv','rb')
-		ftp.storbinary('STOR '+lang+'wp_articles.csv', file)
+		ftp.storbinary('STOR '+lang+'wquote_articles.csv', file)
 		file.close()
 		ftp.quit()
