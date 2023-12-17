ca67c7eb048b4fdfe48fa265b5d5db554db53c23
Mike Peel <email@mikepeel.net>
2023-12-17 11:07:05 +0000

    try to fix

--- query_run_script.sh
+++ query_run_script.sh
@@ -1,1 +1,2 @@
-toolforge jobs run queryrun --image bullseye --command /data/project/pibot/wikicode/query_run.sh
+source /data/project/pibot/venv/bin/activate
+toolforge jobs run queryrun --image python3.11 --command /data/project/pibot/wikicode/query_run.sh
