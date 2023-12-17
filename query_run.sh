456bfec9c41b91741edbcf5348bfb4915d4d7a91
Mike Peel <email@mikepeel.net>
2023-12-17 11:12:55 +0000

    fix?

--- query_run.sh
+++ query_run.sh
@@ -1,4 +1,4 @@
-python3 /data/project/pibot/wikicode/query_infobox_candidates.py
-python3 /data/project/pibot/wikicode/query_enwp_articles_no_wikidata.py
-python3 /data/project/pibot/wikicode/query_enwp_categories_no_wikidata.py
-python3 /data/project/pibot/wikicode/query_wikiquote_articles_no_wikidata.py
+/data/project/pibot/venv/bin/python3 /data/project/pibot/wikicode/query_infobox_candidates.py
+/data/project/pibot/venv/bin/python3 /data/project/pibot/wikicode/query_enwp_articles_no_wikidata.py
+/data/project/pibot/venv/bin/python3 /data/project/pibot/wikicode/query_enwp_categories_no_wikidata.py
+/data/project/pibot/venv/bin/python3 /data/project/pibot/wikicode/query_wikiquote_articles_no_wikidata.py
