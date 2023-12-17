370022a6b01653513e7666d96184954bcbd2bd04
Mike Peel <email@mikepeel.net>
2023-12-17 11:16:22 +0000

    bash

--- query_run.sh
+++ query_run.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+source /data/project/pibot/venv/bin/activate
 /data/project/pibot/venv/bin/python3 /data/project/pibot/wikicode/query_infobox_candidates.py
 /data/project/pibot/venv/bin/python3 /data/project/pibot/wikicode/query_enwp_articles_no_wikidata.py
 /data/project/pibot/venv/bin/python3 /data/project/pibot/wikicode/query_enwp_categories_no_wikidata.py
