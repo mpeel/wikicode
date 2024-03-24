43d43f6b76f5f35f1d57b64387c69b9cbdccac29
Mike Peel <email@mikepeel.net>
2024-03-24 17:45:37 +0000

    add script

---
+++ qic_count_nom_vote.py
@@ -0,0 +1,98 @@
+import pywikibot
+import re
+
+commons = pywikibot.Site('commons', 'commons')
+
+pagename = 'Commons:Quality images candidates/candidate list'
+page = pywikibot.Page(commons, pagename)
+text = page.get()
+
+userRE = re.compile("\[\[[Uu]ser:([^\|\]]+)[^\]]*\]\]")
+
+nominators = {}
+reviewers = {}
+
+inGallery = False
+inConsensual = 0
+for line in text.split("\n"):
+	if line[:8] == "<gallery" and inGallery == False:
+		inGallery = True
+	elif line == "</gallery>" and inGallery == True:
+		inGallery = False
+	elif line == "= Consensual review =" and inConsensual == 0:
+		inConsensual = 1
+
+	if inGallery and line[:8] != "<gallery" and len(line) > 0:
+		line = line.replace('By [[User', '')
+		line_parts = line.split("|")
+		# print(line_parts)
+		user = userRE.search(line)
+		# print(user)
+		nominator = user.group(1)
+		try:
+			nominators[nominator] = nominators[nominator] + 1
+			# print(nominators[nominator])
+		except:
+			nominators[nominator] = 1
+		# print(nominators)
+		next_test = line.split(user.group(1))[-1]
+		# print(next_test)
+		searchresult = userRE.search(next_test)
+		if searchresult != None:
+			reviewer = searchresult.group(1)
+			try:
+				reviewers[reviewer] = reviewers[reviewer] + 1
+				# print(reviewers[reviewer])
+			except:
+				reviewers[reviewer] = 1
+			# print(user2.group(1))
+
+
+
+	if inConsensual:
+		if "{{/" in line:
+			line = line.replace('By [[User', '')
+			line_parts = line.split("|")
+			# print(line_parts)
+			user = userRE.search(line)
+			# print(user)
+			nominator = user.group(1)
+			try:
+				nominators[nominator] = nominators[nominator] + 1
+				# print(nominators[nominator])
+			except:
+				nominators[nominator] = 1
+		else:
+			searchresult = userRE.search(line)
+			if searchresult != None:
+				reviewer = searchresult.group(1)
+				try:
+					reviewers[reviewer] = reviewers[reviewer] + 1
+					# print(reviewers[reviewer])
+				except:
+					reviewers[reviewer] = 1
+
+report_page = "This page reports statistics for [[Commons:Quality images candidates/candidate list]]. It is automatically maintained by [[User:Pi bot]], please do not edit it!\n\n{| class=\"wikitable sortable\"\n!User!!Nominations!!Reviews"
+
+users_done = []
+for key, value in nominators.items():
+	print(key + " " + str(value))
+	users_done.append(key)
+	try:
+		review_count = reviewers[key]
+	except:
+		review_count = 0
+	if review_count >= value:
+		report_page = report_page + '\n|- style="background-color:lightgreen'
+	else:
+		report_page = report_page + '\n|-'
+	report_page = report_page + '\n| ' + key + ' || ' + str(value) + ' || ' + str(review_count)
+for key, value in reviewers.items():
+	if key not in users_done:
+		report_page = report_page + '\n|- style="background-color:lightgreen'
+		report_page = report_page + '\n| ' + key + ' || 0 || ' + str(value)
+		# print(key + " " + str(value))
+# print(nominators)
+# print(reviewers)
+report_page = report_page + '\n|}'
+print(report_page)
