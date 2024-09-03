c43c8d7c8823b920ed4c8928a3602323c00a0b77
Mike Peel <email@mikepeel.net>
2024-09-03 20:09:55 +0200

    temporary query

--- query_infobox_candidates.py
+++ query_infobox_candidates.py
@@ -23,17 +23,35 @@ step = 10
 run = True
 with conn.cursor() as cur:
 	cur.execute('use commonswiki_p')
-	cur.execute("SELECT DISTINCT pp.pp_value, p1.page_title"\
-	" FROM categorylinks AS c1"\
-	" JOIN page AS p1 ON c1.cl_from=p1.page_id AND p1.page_namespace=14 AND p1.page_is_redirect=0"\
-	" JOIN page_props AS pp ON pp.pp_page = p1.page_id AND pp.pp_propname = 'wikibase_item'"\
-	" LEFT JOIN templatelinks AS tl ON tl.tl_from = p1.page_id AND tl.tl_from_namespace = 14 AND  tl_target_id = (SELECT lt_id FROM linktarget WHERE lt_namespace = 10 AND lt_title = 'Wikidata_Infobox')"\
-	" WHERE tl.tl_target_id IS NULL")
+	cur.execute("SELECT"\
+	" page_title,"
+	" (COUNT( eu_entity_id )) as ct"
+	" FROM"
+	"  page,"
+	"  page_props,"
+	"  wbc_entity_usage"
+	" WHERE"
+	"  pp_propname = \"unexpectedUnconnectedPage\""
+	"  AND page_id = pp_page"
+	"  AND page_namespace = 14"
+	"      AND NOT EXISTS"
+	"        ("
+	"        SELECT  null "
+	"        FROM    templatelinks"
+	"        WHERE   page_id = tl_from"
+	"        )  "
+	"  AND page_id = eu_page_id"
+	"  AND eu_aspect = \"S\""
+	"  /* AND page_title = \"Naupliastraße_(München)\" */"
+	" GROUP BY"
+	"  page_title"
+	" HAVING"
+	"  ct = 1")
 	vals = cur.fetchall()
 	f = open("/data/project/pibot/commons_infobox_candidates.csv", "w", encoding='utf-8')
 	if len(vals) > 0:
 		start += step
-		[f.write(str(x[1])+'\n') for x in vals]
+		[f.write(str(x[0])+'\n') for x in vals]
 	else:
 		run = False
 	f.close()
