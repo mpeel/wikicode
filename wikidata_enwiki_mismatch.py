948b7fd5de8ee2c90a2f2afa1c723b5bff1314e4
Mike Peel <email@mikepeel.net>
2024-09-10 20:58:40 +0100

    fix

--- wikidata_enwiki_mismatch.py
+++ wikidata_enwiki_mismatch.py
@@ -19,7 +19,7 @@ today = now.strftime('%Y-%m-%d')
 
 f = open("wikidata_enwiki_mismatch_"+today+".csv", "w", encoding='utf-8')
 # f.write('statement_guid,property_id,wikidata_value,external_value,external_url\n')
-f.write('item_id,statement_guid,property_id,wikidata_value,meta_wikidata_value,external_value,external_url\n')
+f.write('item_id,statement_guid,property_id,wikidata_value,meta_wikidata_value,external_value,external_url,type\n')
 # print('statement_guid,property_id,wikidata_value,external_value,external_url')
 # print('item_id,statement_guid,property_id,wikidata_value,meta_wikidata_value,external_value,external_url')
 cat = pywikibot.Category(wiki, 'Category:Wikipedia categories tracking Wikidata differences')
@@ -94,7 +94,7 @@ for subcat in pagegenerators.SubCategoriesPageGenerator(cat, recurse=False):
 							compval = clm.getTarget().title()
 					if count == 1 and localid.strip() != compval.strip():
 						# OK, we have a local ID, and a single Wikidata ID, return for mismatch
-						f.write('"'+wd_item.title()+'","'+snakid + '","'+propid+'","'+compval+'",,"'+localid+'","http://en.wikipedia.org/wiki/'+urllib.parse.quote(page.title().replace(' ','_'))+'"\n')
+						f.write('"'+wd_item.title()+'","'+snakid + '","'+propid+'","'+compval+'",,"'+localid+'","http://en.wikipedia.org/wiki/'+urllib.parse.quote(page.title().replace(' ','_'))+'",\n')
 						print('"'+wd_item.title()+'","'+snakid + '","'+propid+'","'+compval+'",,"'+localid+'","http://en.wikipedia.org/wiki/'+urllib.parse.quote(page.title().replace(' ','_')))
 		except:
 			continue
